97439c33f4b5192f635426f0f6b68d2a10377b97
Dmitry Agafonov <agafonovdmitry@gmail.com>
2015-12-20 16:36:38 +0300

    retouch

--- dns_round_robin_test.py
+++ dns_round_robin_test.py
@@ -5,23 +5,43 @@ from time import sleep
 from socket import gethostbyname
 import sys
 
-parser = argparse.ArgumentParser()
-parser.add_argument("name", help="DNS name to test")
-parser.add_argument("-c", "--count", help="set total count of resolves", type=int, default=100)
-parser.add_argument("-d", "--delay", help="set delay between resolves in 1/100s of second", type=int, default=100)
-
-args = parser.parse_args()
-
-data = {}
-
 
 def report(cycle):
     sys.stdout.write("\x1b[2J\x1b[H")
-    print("==== %s ======== %d of %d ====" % (args.name, cycle, args.count))
+    print("==%s== %d/%d" % (
+        (" %s " % args.name).center(40, "="),
+        cycle,
+        args.count
+    ))
     for ip in data:
         perc = data[ip] * 100.0 / cycle
-        print("%16s [%20s %03.1f%%] %d" % (ip, "|" * int(perc / 5.0), perc, data[ip]))
+        print("%15s [%20s %04.1f%%] %d" % (
+            ip, "|" * int(perc / 5.0), perc, data[ip]
+        ))
 
+
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "name",
+    help="DNS name to test"
+)
+parser.add_argument(
+    "-c",
+    "--count",
+    help="set total count of resolves",
+    type=int,
+    default=100
+)
+parser.add_argument(
+    "-d",
+    "--delay",
+    help="set delay between resolves in 1/100s of second",
+    type=int,
+    default=100
+)
+args = parser.parse_args()
+
+data = {}
 
 for cycle in range(0, args.count):
     if cycle:
