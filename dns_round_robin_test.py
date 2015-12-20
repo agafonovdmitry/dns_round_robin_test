47212937f9358d61d11aa3ae1bbb0d6565fa6629
Dmitry Agafonov <agafonovdmitry@gmail.com>
2015-12-20 16:17:43 +0300

    make all thigs simper and correct

--- dns_round_robin_test.py
+++ dns_round_robin_test.py
@@ -19,7 +19,8 @@ def report(cycle):
     sys.stdout.write("\x1b[2J\x1b[H")
     print("==== %s ======== %d of %d ====" % (args.name, cycle, args.count))
     for ip in data:
-        print("%16s [%20s] %d" % (ip, "|" * int(data[ip][1] / 5), data[ip][0]))
+        perc = data[ip] * 100.0 / cycle
+        print("%16s [%20s %03.1f%%] %d" % (ip, "|" * int(perc / 5.0), perc, data[ip]))
 
 
 for cycle in range(0, args.count):
@@ -27,8 +28,7 @@ for cycle in range(0, args.count):
         sleep(args.delay / 100.0)
     ip = gethostbyname(args.name)
     if ip in data:
-        data[ip][0] += 1
+        data[ip] += 1
     else:
-        data[ip] = [1, 0]
-    data[ip][1] = int(data[ip][0] * 100.0 / (cycle + 1))
+        data[ip] = 1
     report(cycle + 1)
